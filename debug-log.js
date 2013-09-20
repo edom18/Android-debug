d8b073290beaa1b5cbef650a4e4cd87a552d5574
edom18 <edo.m18@gmail.com>
2013-09-20 12:39:45 +0900

    update

--- debug-log.js
+++ debug-log.js
@@ -2,121 +2,165 @@
 
     'use strict';
 
-    var debugLogEl = doc.createElement('div'),
-        style = debugLogEl.style;
-
     var isTouch = 'ontouchstart' in window,
         MOUSE_DOWN = isTouch ? 'touchstart' : 'mousedown',
         MOUSE_MOVE = isTouch ? 'touchmove'  : 'mousemove',
         MOUSE_UP   = isTouch ? 'touchend'   : 'mouseup';
 
-    style.cssText = [
-        'position: absolute;',
-        'left: 0;',
-        'top: 0;',
-        'z-index: 1000;',
-        'width: 100%;',
-        'box-sizing: border-box;',
-        'overflow: auto;',
-        'color: white;',
-        'line-height: 1.3;',
-        'border: solid 1px #333;',
-        'background: rgba(0, 0, 0, 0.7);',
-        'padding: 5px;'
-    ].join('');
+    function _addListener(node, type, callback, context) {
 
-    function addListener(node, type, cb) {
+        function _handler(e) {
+            callback.apply(context, e);
+        }
+
         if (node.addEventListener) {
-            node.addEventListener(type, cb, false);
+            node.addEventListener(type, _handler, false);
         }
         else {
-            node.attachEvent('on' + type, cb);
+            node.attachEvent('on' + type, _handler);
         }
     }
 
-    var flg = false;
-    addListener(debugLogEl, MOUSE_DOWN, function (e) {
-        flg = true;
-    });
-    addListener(debugLogEl, MOUSE_MOVE, function (e) {
-        flg = false;
-    });
-    addListener(debugLogEl, MOUSE_UP, function (e) {
-        if (flg === true) {
-            debugLogEl.parentNode.removeChild(debugLogEl);
-        }
-    });
-
-    //Attach an event.
-    doc.body.appendChild(debugLogEl);
-
-    function _log(mes, type) {
-        var exStyle = '';
-        var logMessage = '';
-
-        if (type === 'error') {
-            exStyle = 'color: red;';
-        }
-
-        //Escape html strings.
-        mes = mes.replace(/\&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-
-        logMessage = '<p style="border-bottom: solid 1px #777; padding-bottom: 2px; margin-bottom: 2px; ' + exStyle + '">' + mes + '</p>';
-        debugLogEl.innerHTML =  logMessage + debugLogEl.innerHTML;
+    /**
+     * DebugLogger class.
+     * @class
+     * @constructor
+     */
+    function DebugLogger() {
+        this._init();
     }
 
-    function _checkObj(obj) {
-        var res = [],
-            resobj = [];
-
-        var type = {}.toString.call(obj);
-
-        if (!(type === '[object Object]' || type === '[object Array]')) {
-            return [obj];
-        }
-
-        for (var key in obj) {
-            if (typeof obj[key] === 'object') {
-                resobj = _checkObj(obj[key]);
-                res.push(key + ': {' + resobj.join(', ') + '}');
-            }
-            else {
-                res.push(key + ': ' + obj[key]);
-            }
-        }
-
-        return res;
-    }
-
-    function _dump(obj) {
-        var mes = _checkObj(obj);
-        return mes.join(', ');
-    }
-
-    function log() {
-        var mes = [];
-        for (var i = 0, l = arguments.length; i < l; i++) {
-            mes.push(_dump(arguments[i]));
-        }
-        _log(mes.join(''));
-    }
-
-    function error() {
-        var mes = [];
-        for (var i = 0, l = arguments.length; i < l; i++) {
-            mes.push(_dump(arguments[i]));
-        }
-        _log(mes.join(''), 'error');
-    }
-
-    function clear() {
-        debugLogEl.innerHTML = '';
-    }
-
-    exports.debug = {
-        log  : log,
-        error: error,
-        clear: clear
+    DebugLogger.type = {
+        ERROR: 1
     };
+
+    DebugLogger.prototype = {
+        constructor: DebugLogger,
+
+        /*! ------------------------------------------------------
+            PRIVATE METHODS
+        ---------------------------------------------------------- */
+        _init: function () {
+            this.el = doc.createElement('div');
+
+            this.el.style.cssText = [
+                'position: absolute;',
+                'left: 0;',
+                'top: 0;',
+                'z-index: 1000;',
+                'width: 100%;',
+                'box-sizing: border-box;',
+                'overflow: auto;',
+                'color: white;',
+                'line-height: 1.3;',
+                'border: solid 1px #333;',
+                'background: rgba(0, 0, 0, 0.7);',
+                'padding: 5px;'
+            ].join('');
+
+
+            //Attach an event.
+            _addListener(this.el, MOUSE_DOWN, this._mDown, this);
+            _addListener(this.el, MOUSE_MOVE, this._mMove, this);
+            _addListener(this.el, MOUSE_UP,   this._mUp,   this);
+
+            //Append element to the body.
+            document.body.appendChild(this.el);
+        },
+
+        _mDown: function (e) {
+            this.flg = true;
+        },
+        _mMove: function (e) {
+            this.flg = false;
+        },
+        _mUp: function (e) {
+            if (this.flg === true) {
+                this.el.parentNode.removeChild(this.el);
+            }
+
+            this.flg = false;
+        },
+
+        /**
+         * @param {string} mes To show message.
+         * @param {enum.<number>} type Log type.
+         */
+        _log: function (mes, type) {
+            var exStyle = '';
+            var logMessage = '';
+
+            if (type === DebugLogger.type.ERROR) {
+                exStyle = 'color: red;';
+            }
+
+            //Escape html strings.
+            mes = mes.replace(/\&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+            logMessage = '<p style="border-bottom: solid 1px #777; padding-bottom: 2px; margin-bottom: 2px; ' + exStyle + '">' + mes + '</p>';
+            this.el.innerHTML =  logMessage + this.el.innerHTML;
+        },
+
+        _checkObj: function (obj) {
+
+            var res = [],
+                resobj = [];
+
+            var type = {}.toString.call(obj);
+
+            if (!(type === '[object Object]' || type === '[object Array]')) {
+                return [obj];
+            }
+
+            for (var key in obj) {
+                if (typeof obj[key] === 'object') {
+                    resobj = _checkObj(obj[key]);
+                    res.push(key + ': {' + resobj.join(', ') + '}');
+                }
+                else {
+                    res.push(key + ': ' + obj[key]);
+                }
+            }
+
+            return res;
+        },
+
+        _dump: function (obj) {
+            var mes = this._checkObj(obj);
+            return mes.join(', ');
+        },
+
+        /*! ------------------------------------------------------
+            PUBLIC METHODS
+        ---------------------------------------------------------- */
+        log: function () {
+            var mes = [];
+
+            for (var i = 0, l = arguments.length; i < l; i++) {
+                mes.push(this._dump(arguments[i]));
+            }
+
+            this._log(mes.join(''));
+        },
+
+        error: function () {
+            var mes = [];
+
+            for (var i = 0, l = arguments.length; i < l; i++) {
+                mes.push(this._dump(arguments[i]));
+            }
+
+            this._log(mes.join(''), DebugLogger.type.ERROR);
+        },
+
+        clear: function () {
+            this.el.innerHTML = '';
+        }
+    };
+
+    /*! -------------------------------------------------------------
+        EXPORTS
+    ----------------------------------------------------------------- */
+    exports.DebugLogger = DebugLogger;
 
 }(window, document, window));
